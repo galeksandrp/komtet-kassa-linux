674c2e4545354a976a866c3fc5aff3d517575977
Alexander Georgievskiy <galeksandrp@gmail.com>
2021-11-13 07:03:49 +0300

    feature: extensive logging

--- komtet_kassa_linux/__init__.py
+++ komtet_kassa_linux/__init__.py
@@ -7,6 +7,9 @@ from komtet_kassa_linux.km_manager import run as run_manager
 from komtet_kassa_linux.web import run as run_web
 
 
+if True: # utilsGetLoggingAvailability
+    logging.basicConfig(level=logging.DEBUG)
+
 logger = logging.getLogger(__name__)
 
 __version__ = get_distribution('komtet_kassa_linux').version
